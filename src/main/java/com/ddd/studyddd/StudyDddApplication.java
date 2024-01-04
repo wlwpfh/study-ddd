c4b945b647d3978978f8a3665cfbb8f3a82b12f8
yang Jiyoung <0112jyoung@naver.com>
2024-01-04 21:54:24 +0900

    feat: 초기 설정

---
+++ src/main/java/com/ddd/studyddd/StudyDddApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.studyddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyDddApplication.class, args);
+    }
+
+}
