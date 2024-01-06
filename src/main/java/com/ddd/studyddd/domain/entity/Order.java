c47db4b101b0bf519b726b0bc34d807ed05be8e8
yang Jiyoung <0112jyoung@naver.com>
2024-01-06 18:59:18 +0900

    feat: 도메인 모델 도출

--- src/main/java/com/ddd/studyddd/domain/entity/Order.java
+++ src/main/java/com/ddd/studyddd/domain/entity/Order.java
@@ -1,20 +1,74 @@
 package com.ddd.studyddd.domain.entity;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Order {
     private OrderState state;
     private ShippingInfo shippingInfo;
 
-    public void chagneShippingInfo(ShippingInfo newShippingInfo){
-        if(!isShippingChangeable()){
-          throw new IllegalStateException("Can't change shipping in " + state);
-        }
-        this.shippingInfo = newShippingInfo;
+    private List<OrderLine> orderLines;
+    private Money totalAmounts;
+
+    public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
+        setOrderLines(orderLines);
+        setShippingInfo(shippingInfo);
     }
 
-    public boolean isShippingChangeable(){
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyAtLeastOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void setShippingInfo(ShippingInfo shippingInfo) {
+        if (Objects.isNull(shippingInfo)) {
+            throw new IllegalArgumentException("no ShippingInfo");
+        }
+        this.shippingInfo = shippingInfo;
+    }
+
+    private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
+        if (Objects.isNull(orderLines) || orderLines.isEmpty()) {
+            throw new IllegalArgumentException("no OrderLine");
+        }
+    }
+
+    private void calculateTotalAmounts() {
+        int sum = orderLines.stream().mapToInt(x -> x.getAmounts())
+                .sum();
+        this.totalAmounts = new Money(sum);
+    }
+
+    public void chagneShippingInfo(ShippingInfo newShippingInfo) {
+        verifyNotYetShipped();
+        setShippingInfo(newShippingInfo);
+    }
+
+    public void changeShipped() {
+
+    }
+
+    public void cancel() {
+        verifyNotYetShipped();
+        this.state = OrderState.CANCELED;
+    }
+
+    public void completePayment() {
+
+    }
+
+    private void verifyNotYetShipped() {
+        if (state != OrderState.PAYMENT_WAITING && state != OrderState.PREPARING) {
+            throw new IllegalStateException("already shipped");
+        }
+    }
+
+    private boolean isShippingChangeable() {
         return state == OrderState.PAYMENT_WAITING || state == OrderState.PREPARING;
     }
+
     public enum OrderState {
-        PAYMENT_WAITING, PREPARING, SHIPPED, DELIVERING, DELIVERY_COMPLETED;
+        PAYMENT_WAITING, PREPARING, SHIPPED, DELIVERING, DELIVERY_COMPLETED, CANCELED;
     }
 }
--- src/main/java/com/ddd/studyddd/domain/entity/ShippingInfo.java
+++ src/main/java/com/ddd/studyddd/domain/entity/ShippingInfo.java
@@ -1,4 +1,12 @@
 package com.ddd.studyddd.domain.entity;
 
+import lombok.Getter;
+
+@Getter
 public class ShippingInfo {
+    private String receiverName;
+    private String receiverPhoneNumber;
+    private String shippingAddress1;
+    private String shippingAddress2;
+    private String shippingAddress3;
 }
