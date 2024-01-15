8ecef4ceccd966435fd9eebe9b9a2552dab126a8
yang Jiyoung <0112jyoung@naver.com>
2024-01-15 22:33:37 +0900

    feat: DIP

--- src/main/java/com/ddd/studyddd/CalculateDiscountService.java
+++ src/main/java/com/ddd/studyddd/CalculateDiscountService.java
@@ -2,6 +2,7 @@ package com.ddd.studyddd;
 
 import com.ddd.studyddd.domain.entity.Money;
 import com.ddd.studyddd.domain.entity.OrderLine;
+import lombok.CustomLog;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
@@ -9,15 +10,21 @@ import java.util.List;
 
 @RequiredArgsConstructor
 public class CalculateDiscountService {
-    private DroolsRuleEngine ruleEngine;
+    private final DroolsRuleEngine ruleEngine;
+    private final RuleDiscounter ruleDiscounter;
+    private final CustomerRepository customerRepository;
 
     public Money calculateDiscount(List<OrderLine> orderLines, String customerId){
         Customer customer = findCustomer(customerId);
 
-        MutableMoney money = new MutableMoney(0);
-        List<?> facts = Arrays.asList(customer, money);
-        facts.addAll(orderLines);
-        ruleEngine.evaluate("discountCalculation", facts);
-        return money.toImmutableMoney();
+        return ruleDiscounter.applyRules(customer, orderLines);
+    }
+
+    private Customer findCustomer(String customerId){
+        Customer customer = customerRepository.findById(customerId);
+        if(customer == null){
+            throw new NoCustomerException();
+        }
+        return customer;
     }
 }
---
+++ src/main/java/com/ddd/studyddd/DroolsRuleDiscounter.java
@@ -0,0 +1,25 @@
+package com.ddd.studyddd;
+
+import com.ddd.studyddd.domain.entity.Money;
+import com.ddd.studyddd.domain.entity.OrderLine;
+
+import java.util.List;
+
+public class DroolsRuleDiscounter implements RuleDiscounter {
+    private KieContainer kieContainer;
+
+    public DroolsRuleEngine(){
+        KieServices ks = KieServices.Factory.get();
+        kieContainer = ks.getKieClasspathContainer();
+    }
+    @Override
+    public Money applyRules(Customer customer, List<OrderLine> orderLines) {
+        KieSession kSession = kieContainer.newKieSession("discountSession");
+        try{
+            kSession.fireAllRules();
+        } finally {
+            kSession.dispose();
+        }
+        return money.toImmutableMoney();
+    }
+}
